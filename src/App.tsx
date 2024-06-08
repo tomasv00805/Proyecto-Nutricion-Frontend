6590e2be28668dd2a9479cd4d6068a30ddf9a4d9
luchossj27 <luchossj27@gmail.com>
2024-06-08 20:39:26 +0000

    thanks page deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './Styles/App.css';
 import Formulario from './Components/Formulario.tsx'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.tsx';
-import MandeForm from './Pages/MandeForm.tsx';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<Formulario />} />
-        <Route path="/form/enviado" element={<MandeForm />} />
         </Routes>
       </div>
     </Router>
